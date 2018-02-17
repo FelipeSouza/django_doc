fa096e7b4312f642a26163f48a627622ac4d3119
FelipeSouza <fso.martins@gmail.com>
2018-02-17 20:43:31 -0200

    Register the objects Question and Choice in admin site

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question, Choice
+
+# add nossas tabelas no admin
+
+admin.site.register(Question)
+admin.site.register(Choice)
